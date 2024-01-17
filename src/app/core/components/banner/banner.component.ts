ed22bfe49395d449cddce2f6b42600d2796994ad
ferio48 <pranav212salaria@gmail.com>
2024-01-17 17:32:36 +0530

    Browse_Page_Complete_17/1/24

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-banner',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
-export class BannerComponent {
+export class BannerComponent implements OnChanges{
+  @Input({required: true}) bannerTitle = '';
+  @Input() bannerOverview = '';
+  @Input() key = 'r_pUE7OcN8w';
+  private sanitizer = inject(DomSanitizer)
+  videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0&playlist=${this.key}`)
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['key']){
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0&playlist=${this.key}`);
+    }
+  }
 }
